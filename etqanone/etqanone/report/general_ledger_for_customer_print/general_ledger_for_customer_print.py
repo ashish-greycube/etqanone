2d11f1cb82f76a5aaed7319f3e082c6705029865
ashish-greycube <mr.ashish.shah@gmail.com>
2024-02-14 12:21:52 +0530

    feat: remarks for voucher type in print

--- etqanone/etqanone/report/general_ledger_for_customer_print/general_ledger_for_customer_print.py
+++ etqanone/etqanone/report/general_ledger_for_customer_print/general_ledger_for_customer_print.py
@@ -13,5 +13,21 @@ def execute(filters=None):
 		filters.party = frappe.parse_json(filters.get("party"))
 	else:
 		frappe.throw(_("Select a customer in party field"))
-
-	return execute(filters)
+	data =execute(filters)
+	for res in data:
+		for row in res:
+			if row.get('gl_entry'):
+				if row.get('voucher_type')=='Sales Invoice':
+					row['remarks']=row.get('voucher_no')
+					is_return=frappe.db.get_value(row.get('voucher_type'), row.get('voucher_no'), 'is_return')
+					if is_return==1:
+						row['voucher_type']='Sales Return'
+				elif row.get('voucher_type')=='Payment Entry':
+					remark=frappe.db.get_value(row.get('voucher_type'), row.get('voucher_no'), 'remark')
+					if remark:
+						row['remarks']=remark
+				elif row.get('voucher_type')=='Journal Entry':
+					remark=frappe.db.get_value(row.get('voucher_type'), row.get('voucher_no'), 'jv_remark')
+					if remark:
+						row['remarks']=remark						
+	return data
