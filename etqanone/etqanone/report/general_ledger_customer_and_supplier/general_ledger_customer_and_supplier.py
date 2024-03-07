640934739ab2f887d180b0ca4752b35b7cbc9cc3
ashish-greycube <mr.ashish.shah@gmail.com>
2024-03-07 14:08:56 +0530

    feat: GL for supplier linked with customer

---
+++ etqanone/etqanone/report/general_ledger_customer_and_supplier/general_ledger_customer_and_supplier.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2024, Greycube and contributors
+# For license information, please see license.txt
+
+from collections import OrderedDict
+
+import frappe
+from frappe import _, _dict
+from frappe.utils import cstr, getdate
+from six import iteritems
+
+from erpnext import get_company_currency, get_default_company
+from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
+	get_accounting_dimensions,
+	get_dimension_with_children,
+)
+from erpnext.accounts.report.financial_statements import get_cost_centers_with_children
+from erpnext.accounts.report.utils import convert_to_presentation_currency, get_currency
+from erpnext.accounts.utils import get_account_currency
+import pandas as pd
+# to cache translations
+TRANSLATIONS = frappe._dict()
+from erpnext.accounts.report.general_ledger import general_ledger
+
+
+def execute(filters=None):
+	print(filters.get('party'))
+	if not filters.get('party'):
+		return
+	
+	if filters.get('party'):
+		customer=filters.get('party')
+		linked_supplier=frappe.db.get_value('Customer', customer, 'linked_customer_supplier_cf')
+		if not linked_supplier:
+			frappe.throw(_("There is no supplier linked with selected customer"))
+		filters['party']=[customer]
+	customer_columns, customer_res = general_ledger.execute(filters)
+
+	if linked_supplier:
+		filters['party_type']='Supplier'
+		filters['party']=[linked_supplier]
+		supplier_columns, supplier_res = general_ledger.execute(filters)
+		res = customer_res + supplier_res
+
+	account_fields=['debit','credit','debit_in_account_currency','credit_in_account_currency','balance']
+	account_opening_dict=frappe._dict({'account': "'Opening'", 'debit': 0.0, 'credit': 0.0, 'debit_in_account_currency': 0.0, 'credit_in_account_currency': 0.0, 'balance': 0.0, 'account_currency': 'SAR', 'bill_no': ''})
+	account_total_dict=frappe._dict({'account': "'Total'", 'debit': 0.0, 'credit': 0.0, 'debit_in_account_currency': 0.0, 'credit_in_account_currency': 0.0, 'balance': 0.0, 'account_currency': 'SAR', 'bill_no': ''})
+	account_closing_dict=frappe._dict({'account': "'Closing (Opening + Total)'", 'debit': 0.0, 'credit': 0.0, 'debit_in_account_currency': 0.0, 'credit_in_account_currency': 0.0, 'balance': 0.0, 'account_currency': 'SAR', 'bill_no': ''})
+	gl_list=[]
+	for row in res:
+		if row['account']=="'Opening'":
+			for account_field in account_fields:
+				account_opening_dict[account_field]=account_opening_dict[account_field]+row[account_field]
+		if row['account']=="'Total'":
+			for account_field in account_fields:
+				account_total_dict[account_field]=account_total_dict[account_field]+row[account_field]
+		if row['account']=="'Closing (Opening + Total)'":
+			for account_field in account_fields:
+				account_closing_dict[account_field]=account_closing_dict[account_field]+row[account_field]
+		if row.get('gl_entry'):
+			gl_list.append(row)
+
+	gl_list.sort(key=lambda x: x['posting_date'], reverse=False)
+	current_balance=account_opening_dict.get('balance')
+	for gl in gl_list:
+		gl['balance']=current_balance+gl['debit']-gl['credit']
+		current_balance=gl['balance']
+	print(customer_columns,'----',supplier_columns)
+	return customer_columns, [account_opening_dict]+gl_list+[account_total_dict]+[account_closing_dict]
--- etqanone/migrations.py
+++ etqanone/migrations.py
@@ -11,6 +11,7 @@ def after_migrations():
 		import_folder_path="{bench_path}/{app_folder_path}".format(bench_path=get_bench_path(),app_folder_path='/apps/etqanone/etqanone/import_records')
 		make_records(import_folder_path,fname)
 	after_migrate_create_si_records()	
+	after_migrate_create_customer_supplier_link_fields()
 	
 
 def make_records(path, fname):
@@ -19,26 +20,70 @@ def make_records(path, fname):
 
 
 
+def after_migrate_create_customer_supplier_link_fields():
+	from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+	custom_fields = {
+		"Customer": [
+			dict(
+				fieldname="_reference_cf",
+				label="Customer Code",
+				fieldtype="Data",
+				insert_after="customer_name_in_arabic",
+				is_custom_field=1,
+				is_system_generated=0,
+				translatable=0,
+				no_copy=1
+			),			
+			dict(
+				fieldname="is_linked_with_supplier_cf",
+				label="Is Linked With Supplier?",
+				fieldtype="Check",
+				insert_after="represents_company",
+				is_custom_field=1,
+				is_system_generated=0,
+				allow_on_submit=1,
+				translatable=0,
+				no_copy=1
+			),
+			dict(
+				fieldname="linked_customer_supplier_cf",
+				label="Linked Supplier",
+				fieldtype="Link",
+				options="Supplier",
+				insert_after="is_linked_with_supplier_cf",
+				is_custom_field=1,
+				is_system_generated=0,
+				allow_on_submit=1,
+				translatable=0,
+				no_copy=1,
+				depends_on= "eval:doc.is_linked_with_supplier_cf==1"
+			)            
+		]        
+	}
+	print("Creating custom fields for app Etqanone:")
+	for dt, fields in custom_fields.items():
+		print("*******\n %s: " % dt, [d.get("fieldname") for d in fields])
+	create_custom_fields(custom_fields)	
 
 def after_migrate_create_si_records():
-    from frappe.custom.doctype.custom_field.custom_field import create_custom_field
+	from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 
-    create_custom_field(
-        "Sales Invoice",
-        dict(
-            fieldname="accounts_receivable_summary_cf_sb",
-            label="Accounts Receivable Summary",
-            fieldtype="Section Break",
-            insert_after="remarks",
-        ),
-    )
-    create_custom_field(
-        "Sales Invoice",
-        dict(
-            fieldname="accounts_receivable_summary_cf",
-            label="",
-            fieldtype="Long Text",
-            insert_after="accounts_receivable_summary_cf_sb",
-        ),
-    )
-    frappe.db.commit()		
+	create_custom_field(
+		"Sales Invoice",
+		dict(
+			fieldname="accounts_receivable_summary_cf_sb",
+			label="Accounts Receivable Summary",
+			fieldtype="Section Break",
+			insert_after="remarks",
+		),
+	)
+	create_custom_field(
+		"Sales Invoice",
+		dict(
+			fieldname="accounts_receivable_summary_cf",
+			label="",
+			fieldtype="Long Text",
+			insert_after="accounts_receivable_summary_cf_sb",
+		),
+	)
+	frappe.db.commit()		
