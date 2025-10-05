a384162c907bd3a841513f48111c6ea8da29eb7d
ashish-greycube <mr.ashish.shah@gmail.com>
2025-10-05 14:37:36 +0530

    fixL rtl def for v13 and v15

--- etqanone/api.py
+++ etqanone/api.py
@@ -11,7 +11,17 @@ from frappe.utils.pdf import get_pdf
 import json
 from six import string_types
 from frappe.www.printview import get_print_style
-from frappe.utils.jinja_globals import is_rtl
+
+# from frappe.utils.jinja_globals import is_rtl
+# from frappe.utils.jinja import is_rtl
+
+
+def is_rtl(rtl=None):
+	from frappe import local
+
+	if rtl is None:
+		return local.lang in ["ar", "he", "fa", "ps"]
+	return rtl
 
 # @frappe.whitelist()
 # def import_arabic_translation():
